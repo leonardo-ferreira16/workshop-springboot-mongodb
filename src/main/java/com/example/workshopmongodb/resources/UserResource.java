4cbc4363dddefa25454128d8f8e5f004e044b99e
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-10-23 09:53:44 -0300

    UserDTO implementation

--- src/main/java/com/example/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/example/workshopmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.workshopmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongodb.domain.User;
+import com.example.workshopmongodb.dto.UserDTO;
 import com.example.workshopmongodb.services.UserServices;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserServices userServices;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = userServices.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
