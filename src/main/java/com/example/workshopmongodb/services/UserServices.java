84f42e1fafacef9cf3bc274a9a9731780cdf17ef
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-10-23 10:37:37 -0300

    Insert User implementation

--- src/main/java/com/example/workshopmongodb/services/UserServices.java
+++ src/main/java/com/example/workshopmongodb/services/UserServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongodb.domain.User;
+import com.example.workshopmongodb.dto.UserDTO;
 import com.example.workshopmongodb.repository.UserRepository;
 import com.example.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserServices {
 	public User findById(String id){
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(),userDto.getEmail());
 	}
 }
