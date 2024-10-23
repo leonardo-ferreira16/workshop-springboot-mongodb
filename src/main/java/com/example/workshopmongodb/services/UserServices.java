fe4529ece6057b2458343a73fe00ba80b1b25d60
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-10-23 10:24:16 -0300

    findById and Exceptions implementation

--- src/main/java/com/example/workshopmongodb/services/UserServices.java
+++ src/main/java/com/example/workshopmongodb/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.example.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongodb.domain.User;
 import com.example.workshopmongodb.repository.UserRepository;
+import com.example.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,5 +18,10 @@ public class UserServices {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
